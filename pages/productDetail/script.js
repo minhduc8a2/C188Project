2b64385882fc71fdc4a0022d16ff1b311ac2bcdc
minhduc8a2 <105045691+minhduc8a2@users.noreply.github.com>
2023-05-05 18:01:15 +0700

    add clothes description

--- pages/productDetail/script.js
+++ pages/productDetail/script.js
@@ -1,3 +1,72 @@
+const clothesDescription = {
+  "blazer/001":
+    "Áo Blazer HD 001 với chất liệu vải dệt kim . Tone màu xanh than giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/002":
+    "Áo Blazer HD 002 với chất liệu vải dệt kim . Tone màu đen giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/003":
+    "Áo Blazer HD 003 với chất liệu vải dệt kim . Tone màu nâu sáng giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/004":
+    "Áo Blazer HD 004 với chất liệu vải dệt kim dáng cách tân hiện đại . Tone màu đen cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/005":
+    "Áo Blazer HD 005 với chất liệu vải dệt kim . Tone màu kem sang giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/006":
+    "Áo Blazer HD 006 với chất liệu vải dệt kim . Tone màu xanh than giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/007":
+    "Áo Blazer HD 007 với chất liệu vải dệt kim . Tone màu xanh than giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "blazer/008":
+    "Áo Blazer HD 008 với chất liệu vải dệt kim . Tone màu caramel than giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/001":
+    "Áo jacket HD 001 với chất liệu kaki phối với túi hộp và khóa zip tạo nên mẫu áo thời thượng. Tone màu be trendy của năm 2023 giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/002":
+    "Áo jacket HD 002 với chất liệu vải tweet không hề lỗi thời trong lịch sử thời trang đại chúng. Vẫn là tone màu be trendy của năm 2023 giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/003":
+    "Áo jacket HD 003 với chất liệu jeans huyền thoiaj của lang thời trang bởi sự trẻ trung, cá tính. Màu đen huyền thoại giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/004":
+    "Áo jacket HD 004 với chất liệu kaki đơn giản thanh lịch . Màu xanh than thanh lịch giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/005":
+    "Áo jacket HD 005 với chất liệu kaki cùng với khuy to bản giúp chiếc áo trở nên ấn tượng . Tone màu be trendy của năm 2023 giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/006":
+    "Áo jacket HD 006 với chất liệu vải lens trơn . Tone màu trắng basic giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/007":
+    "Áo jacket HD 007 với chất liệu jeans cùng với khóa zip basic . Tone màu trendy của năm 2023 giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jacket/008":
+    "Áo jacket HD 008 với chất liệu jeans cùng với khóa zip basic . Tone màu trendy của năm 2023 giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/001":
+    "Quần pants HD 001 Với chất liệu vải kaki form rộng thoải mái cho người mặc. Tone màu đen cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/002":
+    "Quần pants HD 002 Với chất liệu vải kaki form rộng thoải mái cho người mặc. Tone màu trắng cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/003":
+    "Quần pants HD 003 Với chất liệu vải kaki form rộng thoải mái cho người mặc. Tone màu xanh than cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/004":
+    "Quần pants HD 004 Với chất liệu vải kaki ống đứng thoải mái cho người mặc. Tone màu đen cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/005":
+    "Quần pants HD 005 Với chất liệu vải kaki ống đứng thoải mái cho người mặc, kết hợp với túi hộp trẻ trung. Tone màu kem sữa cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/006":
+    "Quần pants HD 006 Với chất liệu vải kaki form ống đứng thoải mái cho người mặc. Tone màu xám trắng cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/007":
+    "Quần pants HD 007 Với chất liệu vải kaki ống đứng thoải mái cho người mặc. Tone màu xanh đen  cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "pants/008":
+    "Quần pants HD 008 Với chất liệu vải kaki ống đứng thoải mái cho người mặc. Tone màu xanh đen  cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/001":
+    "Quần jeans HD 001 Với chất liệu jeans denim, các dương xẻ ở gối chân tạo cá tính mạnh, form quần ống rộng tạo sự thoải mái cho người mặc.  Tông màu xanh cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/002":
+    "Quần jeans HD 002 Với chất liệu jeans denim, với cạp quần thấp, các túi hộp, form quần ống rộng mang đến vẻ ngoài bụi bặm, cá tính của 90s.  Tông muối tiêu cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/003":
+    "Quần jeans HD 003 Với chất liệu jeans denim, dáng suông, ống đứng, trẻ trung, thanh lịch, form quần ống rộng tạo sự thoải mái cho người mặc.  Tông cam sang chảnh cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/004":
+    "Quần jeans HD 004 Với chất liệu jeans denim, dáng Slim fit tôn dáng.  Tông màu trắng cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/005":
+    "Quần jeans HD 005 Với chất liệu jeans denim, dáng Slim fit tôn dáng.  Tông xanh sang cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/006":
+    "Quần jeans HD 006 Với chất liệu jeans denim, dáng Slim fit tôn dáng.  Tông đen cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/007":
+    "Quần jeans HD 007 Với chất liệu jeans denim, dáng Slim fit tôn dáng.  Tông xanh tối cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+  "jeans/008":
+    "Quần jeans HD 008 Với chất liệu jeans denim, dáng Slim fit tôn dáng.  Tông xám tro cơ bản giúp khách hàng dẫn đầu xu hướng khi lựa chọn áo của HD.",
+}
+
+const clothesDescriptionP = document.querySelector(".clothes-description")
+
 const productTitle = document.querySelector(".product-detail__name")
 
 const productImgMain = document.querySelector(".product-img-main")
@@ -5,6 +74,8 @@ const productImgSub = document.querySelector(".product-img-sub")
 const data = localStorage.getItem("product").split("/")
 const [productType, productCode] = data
 console.log(productType, productCode)
+clothesDescriptionP.innerHTML =
+  clothesDescription[`${productType}/${productCode}`]
 productTitle.innerHTML = productType + " " + productCode
 productImgMain.innerHTML = `
               <img
